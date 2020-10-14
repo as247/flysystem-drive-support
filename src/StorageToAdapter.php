a82b0a16e2faaddad48ccd9611f8d8f6bae81240
as247 <as247@vui360.com>
2020-10-14 08:44:20 +0700

    Normalize on setPathPrefix Improve debug

--- src/StorageToAdapter.php
+++ src/StorageToAdapter.php
@@ -14,6 +14,8 @@ use As247\CloudStorages\Exception\UnableToDeleteFile;
 use As247\CloudStorages\Exception\UnableToMoveFile;
 use As247\CloudStorages\Exception\UnableToReadFile;
 use As247\CloudStorages\Exception\UnableToWriteFile;
+use As247\CloudStorages\Storage\GoogleDrive;
+use As247\CloudStorages\Storage\OneDrive;
 use League\Flysystem\Util;
 use function GuzzleHttp\Psr7\stream_for;
 use League\Flysystem\Config;
@@ -25,21 +27,25 @@ trait StorageToAdapter
 	 * @var StorageContract
 	 */
 	protected $storage;
-	protected $throwException=false;
+	protected $throwException = false;
+	protected $exceptExceptions = [
+		FileNotFoundException::class,
+	];
 
 	/**
-	 * @return StorageContract
+	 * @return StorageContract|OneDrive|GoogleDrive
 	 */
-	public function getStorage(){
+	public function getStorage()
+	{
 		return $this->storage;
 	}
 
 	/**
 	 * @inheritDoc
 	 */
-	public function write($path, $contents, Config $config=null)
+	public function write($path, $contents, Config $config = null)
 	{
-		return $this->writeStream($path,stream_for($contents),$config);
+		return $this->writeStream($path, stream_for($contents), $config);
 	}
 
 	/**
@@ -48,16 +54,16 @@ trait StorageToAdapter
 	public function writeStream($path, $resource, Config $config)
 	{
 		try {
-			$config=$this->convertConfig($config);
+			$config = $this->convertConfig($config);
 			$this->storage->writeStream($this->applyPathPrefix($path), $resource, $config);
 			return $this->getMetadata($path);
-		}catch (UnableToWriteFile $e){
-			if($this->throwException){
+		} catch (UnableToWriteFile $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
-		}catch (InvalidStreamProvided $e){
-			if($this->throwException){
+		} catch (InvalidStreamProvided $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -69,7 +75,7 @@ trait StorageToAdapter
 	 */
 	public function update($path, $contents, Config $config)
 	{
-		return $this->write($path,$contents,$config);
+		return $this->write($path, $contents, $config);
 	}
 
 	/**
@@ -77,7 +83,7 @@ trait StorageToAdapter
 	 */
 	public function updateStream($path, $resource, Config $config)
 	{
-		return $this->writeStream($path,$resource,$config);
+		return $this->writeStream($path, $resource, $config);
 	}
 
 	/**
@@ -86,12 +92,12 @@ trait StorageToAdapter
 	public function rename($path, $newpath)
 	{
 		try {
-			$path=$this->applyPathPrefix($path);
-			$newpath=$this->applyPathPrefix($newpath);
+			$path = $this->applyPathPrefix($path);
+			$newpath = $this->applyPathPrefix($newpath);
 			$this->storage->move($path, $newpath, $this->convertConfig(new Config()));
 			return true;
-		}catch (UnableToMoveFile $e){
-			if($this->throwException){
+		} catch (UnableToMoveFile $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -104,13 +110,13 @@ trait StorageToAdapter
 	public function copy($path, $newpath)
 	{
 		try {
-			$config=$this->convertConfig( new Config());
-			$path=$this->applyPathPrefix($path);
-			$newpath=$this->applyPathPrefix($newpath);
+			$config = $this->convertConfig(new Config());
+			$path = $this->applyPathPrefix($path);
+			$newpath = $this->applyPathPrefix($newpath);
 			$this->storage->copy($path, $newpath, $config);
 			return true;
-		}catch (UnableToCopyFile $e){
-			if($this->throwException){
+		} catch (UnableToCopyFile $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -122,19 +128,19 @@ trait StorageToAdapter
 	 */
 	public function delete($path)
 	{
-		if($this->isRootPath($path)){
-		    return false;
-        }
+		if ($this->isRootPath($path)) {
+			return false;
+		}
 		try {
 			$this->storage->delete($this->applyPathPrefix($path));
 			return true;
-		}catch (UnableToDeleteFile $e){
-			if($this->throwException){
+		} catch (UnableToDeleteFile $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
-		}catch (FileNotFoundException $e){
-			if($this->throwException){
+		} catch (FileNotFoundException $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -146,19 +152,19 @@ trait StorageToAdapter
 	 */
 	public function deleteDir($dirname)
 	{
-        if($this->isRootPath($dirname)){
-            return false;
-        }
+		if ($this->isRootPath($dirname)) {
+			return false;
+		}
 		try {
 			$this->storage->deleteDirectory($this->applyPathPrefix($dirname));
 			return true;
-		}catch (UnableToDeleteDirectory $e){
-			if($this->throwException){
+		} catch (UnableToDeleteDirectory $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
-		}catch (FileNotFoundException $e){
-			if($this->throwException){
+		} catch (FileNotFoundException $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -171,11 +177,11 @@ trait StorageToAdapter
 	public function createDir($dirname, Config $config)
 	{
 		try {
-			$config=$this->convertConfig($config);
+			$config = $this->convertConfig($config);
 			$this->storage->createDirectory($this->applyPathPrefix($dirname), $config);
 			return $this->getMetadata($dirname);
-		}catch (UnableToCreateDirectory $e){
-			if($this->throwException){
+		} catch (UnableToCreateDirectory $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -187,7 +193,7 @@ trait StorageToAdapter
 	 */
 	public function setVisibility($path, $visibility)
 	{
-		$this->storage->setVisibility($this->applyPathPrefix($path),$visibility);
+		$this->storage->setVisibility($this->applyPathPrefix($path), $visibility);
 		return $this->getMetadata($path);
 	}
 
@@ -204,8 +210,8 @@ trait StorageToAdapter
 	 */
 	public function read($path)
 	{
-		$stream=$this->readStream($path);
-		return ['contents'=>stream_get_contents($stream['stream'])];
+		$stream = $this->readStream($path);
+		return ['contents' => stream_get_contents($stream['stream'])];
 	}
 
 	/**
@@ -214,9 +220,9 @@ trait StorageToAdapter
 	public function readStream($path)
 	{
 		try {
-			return ['stream'=>$this->storage->readStream($this->applyPathPrefix($path))];
-		}catch (UnableToReadFile $e){
-			if($this->throwException){
+			return ['stream' => $this->storage->readStream($this->applyPathPrefix($path))];
+		} catch (UnableToReadFile $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -229,12 +235,12 @@ trait StorageToAdapter
 	public function listContents($directory = '', $recursive = false)
 	{
 
-		$contents=array_values(iterator_to_array($this->storage->listContents($this->applyPathPrefix($directory),$recursive),false));
+		$contents = array_values(iterator_to_array($this->storage->listContents($this->applyPathPrefix($directory), $recursive), false));
 
-		$contents=array_map(function ($v){
-			$v['path']=$this->removePathPrefix($v['path']);
+		$contents = array_map(function ($v) {
+			$v['path'] = $this->removePathPrefix($v['path']);
 			return $v;
-		},$contents);
+		}, $contents);
 		return $contents;
 	}
 
@@ -246,8 +252,8 @@ trait StorageToAdapter
 		try {
 			$meta = $this->storage->getMetadata($this->applyPathPrefix($path));
 			return $meta->toArrayV1();
-		}catch (FileNotFoundException $e){
-			if($this->throwException){
+		} catch (FileNotFoundException $e) {
+			if ($this->shouldThrowException($e)) {
 				throw $e;
 			}
 			return false;
@@ -286,19 +292,45 @@ trait StorageToAdapter
 		return $this->getMetadata($path);
 	}
 
-    public function applyPathPrefix($path)
-    {
-        return Util::normalizePath(parent::applyPathPrefix($path));
-    }
+	public function setPathPrefix($path)
+	{
+		parent::setPathPrefix(Util::normalizePath($path));
+	}
 
-    protected function isRootPath($path){
-        if ($this->applyPathPrefix($path) === $this->applyPathPrefix('')) {
-            return true;
-        }
-        return false;
-    }
+	public function applyPathPrefix($path)
+	{
+		return Util::normalizePath(parent::applyPathPrefix($path));
+	}
 
-    protected function convertConfig(Config $config){
+	protected function isRootPath($path)
+	{
+		if ($this->applyPathPrefix($path) === $this->applyPathPrefix('')) {
+			return true;
+		}
+		return false;
+	}
+
+	protected function convertConfig(Config $config)
+	{
 		return new \As247\CloudStorages\Support\Config();
+	}
+
+	protected function shouldThrowException($e)
+	{
+		if (empty($this->exceptExceptions)) {
+			return $this->throwException;
+		}
+		return !in_array(get_class($e), $this->exceptExceptions);
+	}
+
+	public function setExcerptExceptions($exceptions)
+	{
+		$this->exceptExceptions = $exceptions;
+		return $this;
+	}
+
+	public function getExcerptExceptions()
+	{
+		return $this->exceptExceptions;
 	}
 }
